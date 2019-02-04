df47592ad1584d8b8ba5da683a3096a6fe4c90d0
aurelien <duquenneaurelien@gmail.com>
2019-02-04 16:30:21 +0100

    added routes: team, contact, home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

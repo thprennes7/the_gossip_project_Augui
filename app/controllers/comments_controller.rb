3351d780facabfce9bc92ffad1cfaa62614c8352
Montblanc159 <guillaume.zaugg@gmail.com>
2019-02-07 00:35:07 +0100

    update and delete comment working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
   def edit
+    @current_comment = Comment.find(params[:id])
+  end
+
+  def update
+    @current_comment = Comment.find(params[:id])
+
+    if @current_comment.update(content: params[:content])
+      flash[:success] = "Commentaire mis à jour !"
+      redirect_to gossip_path(params[:gossip_id])
+    else
+      flash[:danger] = "Veuillez remplir correctement le champ."
+      redirect_to edit_gossip_comment_path(params[:gossip_id])
+    end
   end
 
   def index
@@ -19,6 +32,19 @@ class CommentsController < ApplicationController
     else
       flash[:danger] = "Veuillez remplir correctement le champ."
       redirect_to new_gossip_comment_path(params[:gossip_id])
+    end
+  end
+
+  def destroy
+    @current_comment = Comment.find(params[:id])
+
+
+    if @current_comment.destroy
+      flash[:success] = "Le potin a été supprimé !"
+      redirect_to gossip_path(params[:gossip_id])
+    else
+      flash[:danger] = "Erreur."
+      render gossip_path(params[:gossip_id])
     end
   end
 

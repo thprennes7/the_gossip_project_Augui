96fa31c528460659c8ae4066c108ecf9302cd823
Montblanc159 <guillaume.zaugg@gmail.com>
2019-02-07 00:07:11 +0100

    working create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,20 @@ class CommentsController < ApplicationController
   end
 
   def new
+    puts params
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new_comment(params)
+
+    if @comment.save
+      flash[:success] = "Commentaire ajouté !"
+      redirect_to gossip_path(params[:gossip_id])
+    else
+      flash[:danger] = "Veuillez remplir correctement le champ."
+      redirect_to new_gossip_comment_path(params[:gossip_id])
+    end
   end
 
   def show
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ApplicationRecord
   has_many :likes
   has_many :sub_comments
   validates :content, presence: true
+
+  def self.new_comment(params)
+    Comment.new(content: params[:content], user_id: User.last.id, gossip_id: params[:gossip_id] )
+  end
 end

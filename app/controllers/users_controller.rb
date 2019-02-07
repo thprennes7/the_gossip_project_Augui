13d67d0063b1ca0aefc0b0d207939eb5f8e0f19d
Montblanc159 <guillaume.zaugg@gmail.com>
2019-02-07 15:15:56 +0100

    create user working !

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], city_id: params[:city].to_i, age: params[:age], description: params[:description])
+
+    if @user.save
+      flash[:success] = "Profil enregistré !"
+      redirect_to root_path
+    else
+      flash[:danger] = "Qu'est ce que tu fous ? Recommence !"
+      render :new
+    end
   end
 
   def edit
   end
 end
+
+private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   belongs_to :city
   has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
   has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
@@ -14,4 +15,6 @@ class User < ApplicationRecord
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Email non valide" }
   validates :age, presence: true
   validates :description, presence: true
+  validates :password_digest, presence: true
+  has_secure_password
 end
